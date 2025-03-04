d834abcc88f04f328c11b171c83bfc7a2c40a161
Fatishcat <157142616+Fatishcat@users.noreply.github.com>
2025-03-04 10:06:40 -0500

    the changes made for textbook section basics of html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -7,11 +8,55 @@ function App(): React.JSX.Element {
             <header className="App-header">
                 UM COS420 with React Hooks and TypeScript
             </header>
+            <h1>Header</h1>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload. Xander dufour, Hello World! I need to test
                 more changes
             </p>
+            <div>
+                <img src={"./Assets/Guy in jail.jpg"} alt="A picture" />
+            </div>
+            <div>
+                <Button
+                    onClick={() => {
+                        console.log("Hello World!");
+                    }}
+                >
+                    Log Hello World!
+                </Button>
+            </div>
+            <div>
+                <ul>
+                    <li>bungus</li>
+                    <li>bungus</li>
+                    <li>bungus</li>
+                </ul>
+            </div>
+            <div>
+                <Container>
+                    <Row>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "10000px",
+                                    height: "10px",
+                                    backgroundColor: "red",
+                                }}
+                            ></div>
+                        </Col>
+                        <Col>
+                            <div
+                                style={{
+                                    width: "10000px",
+                                    height: "10px",
+                                    backgroundColor: "red",
+                                }}
+                            ></div>
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
